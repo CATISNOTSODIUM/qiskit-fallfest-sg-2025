c745c379f60b60b0ea8adaebe59756dd1651a51c
Thitiwat Kosolpattanadurong <167766536+CATISNOTSODIUM@users.noreply.github.com>
2025-10-20 00:55:45 +0800

    Update footer.tsx

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="grid grid-cols-1 sm:grid-cols-3 gap-6 p-6text-center items-center">
       <div className="text-sm sm:text-base">
-        © Qiskit Fall Fest 2025. All Rights Reserved.
+        © Quantum Fall Fest 2025. All Rights Reserved.
       </div>
 
       <div className="hidden sm:block border-l h-6 mx-auto"></div>
